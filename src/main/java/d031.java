6a8a2089123064fb2ac9952873bcc869e9e4714c
二俣雄樹 <futamatayuki@futamatayuukinoMacBook-Air.local>
2022-07-07 13:56:14 +0900

    first commit

---
+++ src/main/java/d031.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class d031 {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n*60);
+    }
+}
